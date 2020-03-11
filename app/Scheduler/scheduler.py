d3e7b29e8f04c7db084d226118daaddd613469c6
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2020-03-11 14:11:06 +0800

    Update scheduler.py
    scheduler class

--- app/Scheduler/scheduler.py
+++ app/Scheduler/scheduler.py
@@ -10,6 +10,30 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/scheduler'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+class scheduler(db.Model):
+    __tablename__ = 'scheduler'
+
+    tripID = db.Column(db.Integer(4), primary_key=True)
+    facebookID = db.Column(db.Varchar(20), nullable=False)
+    placesOfInterest = db.Column(db.json, nullable=False)
+    startDate = db.Column(db.date, nullable=False)
+    endDate = db.Column(db.date, nullable=False))
+    paymentStatus = db.Column(db.Varchar(10))
+
+    def __init__(self, tripID, facebookID, placesOfInterest, startDate, endDate, paymentStatus):
+        self.tripID = tripID
+        self.facebookID = facebookID
+        self.placesOfInterest = placesOfInterest
+        self.startDate = startDate
+        self.endDate = endDate
+        self.paymentStatus = paymentStatus
+
+    def json(self):
+        return {"tripID": self.tripID, "facebookID": self.facebookID, "placesOfInterest": self.placesOfInterest, "startDate": self.startDate, "endDate": self.endDate, "paymentStatus": self.paymentStatus}
+
+#function to get facebookID (get from session and store in database)
+#function to get all trips of a specific fb user (retrieve)
 #function (GET) places of interest from user's selected POI- placesOfInterest {POI: name, address}
 #function (GET) start date & end date - calendar startDate, endDate
 #Store trip into scheduler database and POST TO UI 
@@ -19,9 +43,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 CORS(app)
-
-
-
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True) 
