08b7342f310e863ac43602743a894ea667ffaa08
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2020-03-11 14:47:09 +0800

    Create scheduler.sql
    Co-Authored-By: theenc <theenc@users.noreply.github.com>

---
+++ databases/scheduler.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 11, 2020 at 06:46 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scheduler`
+--
+CREATE DATABASE IF NOT EXISTS `scheduler` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `scheduler`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scheduler`
+--
+
+DROP TABLE IF EXISTS `scheduler`;
+CREATE TABLE IF NOT EXISTS `scheduler` (
+  `tripID` int(4) NOT NULL,
+  `facebookID` varchar(20) NOT NULL,
+  `placesOfInterest` json NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `paymentStatus` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
