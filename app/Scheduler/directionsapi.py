b4835c03bbcdafd69b617f2fdf82d54c96aa5ac4
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2020-03-19 20:13:04 +0800

    Update directionsapi.py

--- app/Scheduler/directionsapi.py
+++ app/Scheduler/directionsapi.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
+import urllib, json
 
 app = Flask(__name__)
 CORS(app)
 
-#define our api key
+#define our origin, destination, waypoints
 API_KEY = "AIzaSyCVh6H9I5mG7Y3nXkLzjRwKogIhhBhjVkw"
 
 #way points should be a dictionary, list form
-# e.g waypoints = { origin: [A,B, destination], origin: [C,D,destination] }
+#waypoints_dict = { 'origin1': ['A','B', 'destination'], 'origin2': ['C','D','destination'] }
+
 def get_waypoints_list(waypoints_dict):
     store = []
     for things in waypoints_dict:
@@ -28,11 +30,12 @@ def url_create(origin, destination, waypoint_string, API_KEY):
     mydict = {'origin': origin , 'destination': destination }
     GMAPS_URL = "https://maps.googleapis.com/maps/api/directions/json?"
     url = GMAPS_URL + urlencode(mydict, doseq=True) 
-    
-    
     url = url +  '&waypoints=optimize:true' + waypoint_string + '&key=' + API_KEY
     return url
 
-
+url = url_create(origin, destination, waypoint_string, API_KEY)
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data
 
 
