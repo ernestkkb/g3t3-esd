989da3240b49ea0d7e1fae1e1e9ba8083286f204
theenc <tcsim.2018@sis.smu.edu.sg>
2020-03-20 22:35:03 +0800

    Update placesapi.py

--- app/Scheduler/placesapi.py
+++ app/Scheduler/placesapi.py
@@ -1,23 +1,27 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
-
+import urllib
+import urllib.request
+import json
 app = Flask(__name__)
 CORS(app)
 
 #define our api key
 API_KEY = "AIzaSyCc4-LbdsmzRIHaqyO5fmAg0ew6HuC1eL4"
 
-#retrieved from add_trip.php
+#definecity (retrieved from add_trip.php)
 city = "singapore"
-cityplus =  city.replace(" ", "+")
-search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + cityplus + "point+of+interest&language=en&key=" + API_KEY
-print(search_url)
 
-@app.route('/placesapi')
-def get_all_POI():
-    POI = requests.get(search_url)
-    return POI.json()
+def url_create(city, API_KEY):
+    from urllib.parse import urlencode
+    mydict = {'query': city}
+    POI_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+    url = POI_URL + urlencode(mydict, doseq=True) 
+    url = url + "point+of+interest&language=en&key=" + API_KEY
+    return url
 
-if __name__ == "__main__":
-    app.run(debug=True)
+final_url = url_create(city,API_KEY)
+response = urllib.request.urlopen(final_url)
+data = json.loads(response.read())
+print(data['results'])
