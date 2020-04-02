0230370711d6a4d924324ef2117cb2ab7de55571
BrennanSMU <54878506+BrennanSMU@users.noreply.github.com>
2020-04-02 18:43:11 +0800

    Notification docker

--- docker_placesapi/placesapi.py
+++ docker_placesapi/placesapi.py
@@ -77,7 +77,7 @@ def photos_url(API_KEY, photo_reference, maxwidth):
 
 
 if __name__ == "__main__":
-    app.run(port='5008', debug=True)
+    app.run(host='0.0.0.0', port='5008', debug=True)
 
 
 
