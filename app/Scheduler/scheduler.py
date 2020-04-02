a8e24b783d16a7d8d15e01d1c880bb2260d85db6
tohjaslyn <jaslyntoh.2018@sis.smu.edu.sg>
2020-04-02 15:41:31 +0800

    Clean UP

--- app/Scheduler/scheduler.py
+++ app/Scheduler/scheduler.py
@@ -12,13 +12,11 @@ import requests
 
 
 app = Flask(__name__)
-
 #change link to own database directory
 # our database is called scheduler and table is scheduler
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/scheduler'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 CORS(app)
@@ -68,20 +66,6 @@ class scheduler(db.Model):
     def json(self):
         return {"tripID": self.tripID, "tripName": self.tripName, "facebookID": self.facebookID, "placeOfInterest": self.placeOfInterest, "paymentStatus": self.paymentStatus, "day":self.day, "id":self.id}
 
-
-
-@app.route("/package")
-def get_all():
-    all_package = {"package": [package.json() for package in package.query.all()]}
-    return jsonify(all_package)
-
-# @app.route("/package/<string:tripID>")
-# def find_by_tripid(tripID):
-#     trip = package.query.filter_by(tripID=tripID).all()
-#     if trip:
-#         return jsonify(trip.json())
-#     return jsonify({"message": "Trip not found."}), 404
-
 @app.route("/retrieveAllTripID/<string:tripID>")
 def retrieveAllTripID(tripID):
     details = package.query.filter_by(tripID=tripID).all()
@@ -102,6 +86,7 @@ def addTrip(tripID,userID):
             add_POI_for_preplanned(i)
         return jsonify(detailsToReturn,userID)
     return jsonify({"message": "Trip not found."}), 404
+    
 def add_POI_for_preplanned(data):
     #data = {"id":"5", "tripName": "testing","facebookID":"1", "placeOfInterest":{}, "startDate": "2020-03-12", "endDate":"2020-03-15","paymentStatus":"paid", "day":day} # details of book must be sent in body of the request in JSON format. get_json() retrieves the data from the request received.
 
@@ -114,48 +99,6 @@ def add_POI_for_preplanned(data):
         return jsonify({"message": "An error occurred adding the POI."}), 500 # return JSON with HTTP status code 500 - INTERNAL SERVER ERROR if an exception occurs
     
     return jsonify(addnewpoi.json()), 201 # if no errors, return JSON representation of book with HTTP status cde 201 - CREATED
-
-# @app.route("/package/forward/<string:tripID>", methods = ['POST'])
-# def forward_packageID(tripID):
-#     all_package1 ={"package": [package.json() for package in package.query.filterby(tripID=tripID).all()]}
-
-#     """inform Scheduler microservice"""
-#     # default username / password to the borker are both 'guest'
-#     hostname = "localhost" # default broker hostname. Web management interface default at http://localhost:15672
-#     port = 5672 # default messaging port.
-#     # connect to the broker and set up a communication channel in the connection
-#     connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
-#         # Note: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
-#         # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls
-#     channel = connection.channel()
-
-#     # set up the exchange if the exchange doesn't exist
-#     exchangename="package_topic"
-#     channel.exchange_declare(exchange=exchangename, exchange_type='topic')
-
-#     # prepare the message body content
-#     message = json.dumps(all_package1, default=str) # convert a JSON object to a string
-
-#     channel.queue_declare(queue='package', durable=True) # make sure the queue used by the error handler exist and durable
-#     channel.queue_bind(exchange=exchangename, queue='package', routing_key='*.package') # make sure the queue is bound to the exchange
-#     channel.basic_publish(exchange=exchangename, routing_key="scheduler.package", body=message,
-#         properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
-#     )
-#     # channel.basic_publish(exchange=exchangename, routing_key="notification.payment", body=message,
-#     #     properties=pika.BasicProperties(delivery_mode = 2) # make message persistent within the matching queues until it is received by some receiver (the matching queues have to exist and be durable and bound to the exchange)
-#     # )
-#     #print("Package ID sent to Scheduler & Notification microservice.")
-#     # close the connection to the broker
-#     connection.close()
-
-#retrieve trip by tripID, facebookID, day. This function is to get data for google directions
-@app.route("/scheduler/<string:tripID>/<string:facebookID>/<string:day>")
-def find_for_routing(tripID, facebookID, day):
-    trips = scheduler.query.filter_by(tripID=tripID, facebookID=facebookID, day=day).all()
-    tripToReturn = {"trips" : [trip.json() for trip in trips]}
-    if trips:
-        return jsonify(tripToReturn)
-    return jsonify({"message": "Trip not found."}), 404
 
 @app.route("/retrieveAll/<string:facebookID>")
 def retrieveAll(facebookID):
@@ -227,19 +170,6 @@ def forward_trip():
     # close the connection to the broker
     connection.close()
     return ""
-
-
-
-
-#function to get facebookID (get from session and store in database)
-#function to get all trips of a specific fb user (retrieve)
-#function (GET) places of interest from user's selected POI- placeOfInterest {POI: name, address}
-#function (GET) start date & end date - calendar startDate, endDate
-#Create trip into scheduler database and POST TO UI 
-
-
-
-#function to create: SEND (POST)
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True) 
