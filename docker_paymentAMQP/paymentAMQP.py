70662ad59657b8794d10cff8b1186ce9f4bd7098
ernestkkb <ernest.khoo.2018@smu.edu.sg>
2020-04-10 17:30:01 +0800

    testingamqp

--- docker_paymentAMQP/paymentAMQP.py
+++ docker_paymentAMQP/paymentAMQP.py
@@ -20,26 +20,15 @@ url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//')
 url = urlparse(url_str)
 params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
 credentials=pika.PlainCredentials(url.username, url.password))
-
 paymentURL = "https://g3t3-payment.herokuapp.com/makepayment"
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 # set up the exchange if the exchange doesn't exist
 exchangename="exchange_topic"
 channel.exchange_declare(exchange=exchangename, exchange_type='topic')
+print("Hello")
 def receiveTripDetails():
     print("receiveTripDetails function triggered")
-
-
-    # prepare a queue for receiving messages
-    channelqueue = channel.queue_declare(queue='scheduler', durable=True) # '' indicates a random unique queue name; 'exclusive' indicates the queue is used only by this receiver and will be deleted if the receiver disconnects.
-        # If no need durability of the messages, no need durable queues, and can use such temp random queues.
-    queue_name = channelqueue.method.queue
-    channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.scheduler') # bind the queue to the exchange via the key
-        # any routing_key would be matched
-    
-    # set up a consumer and start to wait for coming messages
-    
 
     channelqueue2 = channel.queue_declare(queue='payment.reply', durable=True) # '' indicates a random unique queue name; 'exclusive' indicates the queue is used only by this receiver and will be deleted if the receiver disconnects.
         # If no need durability of the messages, no need durable queues, and can use such temp random queues.
@@ -49,17 +38,8 @@ def receiveTripDetails():
 
     # set up a consumer and start to wait for coming messages
     channel.basic_consume(queue=queue_name2, on_message_callback=reply_callback, auto_ack=True)
-
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
     channel.start_consuming() # an implicit loop waiting to receive messages; it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
 
-def callback(channel, method, properties, body): # required signature for the callback; no return
-    print("Received Trip details")
-    print(json.loads(body))
-    requests.post(paymentURL,json=json.loads(body))
-    print() # print a new line feed
-
-    
 def reply_callback(channel, method, properties, body): # required signature for the callback; no return
     print("Received a Trip ID")
     string1=json.loads(body)
